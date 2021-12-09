3f903776f6aa53aa0b2db941fa58b0c5479bc77a
oldmartijntje <oldmartijntje@gmail.com>
2021-12-09 13:27:16 +0100

    start commit

---
+++ robotArm1/robotArm1/level1.py
@@ -0,0 +1,13 @@
+from RobotArm import RobotArm
+
+robotArm = RobotArm('assessment1')
+robotArm.speed=1
+
+# Jouw python instructies zet je vanaf hier:
+
+
+
+
+
+# Na jouw code wachten tot het sluiten van de window:
+robotArm.wait()
