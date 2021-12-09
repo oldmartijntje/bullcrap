bac58538c8578b7c67b402056ad0832124aab45f
oldmartijntje <oldmartijntje@gmail.com>
2021-12-09 13:33:13 +0100

    level 1 done

--- robotArm1/robotArm1/level1.py
+++ robotArm1/robotArm1/level1.py
@@ -4,8 +4,32 @@ robotArm = RobotArm('assessment1')
 robotArm.speed=1
 
 # Jouw python instructies zet je vanaf hier:
-
-
+def switch():
+    for x in range(3):
+        robotArm.grab()
+        robotArm.moveLeft()
+        robotArm.drop()
+        robotArm.moveRight()
+    robotArm.moveLeft()
+def moveLeft(amount):
+    for z in range(amount): robotArm.moveLeft()
+def moveRight(amount):
+    for z in range(amount): robotArm.moveRight()
+for x in range(4):
+    robotArm.moveRight()
+switch()
+for x in range(3):
+    robotArm.grab()
+    moveLeft(3)
+    robotArm.drop()
+    moveRight(3)
+moveRight(2)
+switch()
+for x in range(3):
+    robotArm.grab()
+    moveRight(5)
+    robotArm.drop()
+    moveLeft(5)
 
 
 
---
+++ robotArm1/robotArm1/level2.py
@@ -0,0 +1,20 @@
+from RobotArm import RobotArm
+
+robotArm = RobotArm('assessment2')
+robotArm.speed=1
+
+# Jouw python instructies zet je vanaf hier:
+
+
+
+# Onderstaande code pas je NIET aan
+moveArm("right", 3);
+moveBlock("left", 3);
+moveBlock("right", 6);
+moveBlock("right", 2);
+moveBlock("right", 6);
+moveBlock("left", 1);
+moveBlock("left", 3);
+
+# Na jouw code wachten tot het sluiten van de window:
+robotArm.wait()
