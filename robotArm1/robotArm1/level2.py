20ff54dd653030d850cdd057cd2596a003c9634f
oldmartijntje <oldmartijntje@gmail.com>
2021-12-09 13:38:33 +0100

    level 2 klaar

--- robotArm1/robotArm1/level2.py
+++ robotArm1/robotArm1/level2.py
@@ -4,10 +4,21 @@ robotArm = RobotArm('assessment2')
 robotArm.speed=1
 
 # Jouw python instructies zet je vanaf hier:
-
-
-
+def moveArm(direction, amount):
+    for x in range(amount):
+        if direction == "right":robotArm.moveRight()
+        elif direction == "left":robotArm.moveLeft()
+def moveBlock(direction, amount):
+    robotArm.grab()
+    for x in range(amount):
+        if direction == "right":robotArm.moveRight()
+        elif direction == "left":robotArm.moveLeft()
+    robotArm.drop()
+    for x in range(amount):
+        if direction == "right":robotArm.moveLeft()
+        elif direction == "left":robotArm.moveRight()
 # Onderstaande code pas je NIET aan
+
 moveArm("right", 3);
 moveBlock("left", 3);
 moveBlock("right", 6);
